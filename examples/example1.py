e69c79301cd48f4ca86f9ba98c46c73ef142916d
Nastya Sharipova <stacy00.ru@gmail.com>
2024-10-16 19:59:16 +0500

    Add files via upload

---
+++ examples/example1.py
@@ -0,0 +1,4 @@
+def example():
+    print ("Hello, World! (c) Sharipova")
+example()
+    
---
+++ examples/example7.py
@@ -0,0 +1,5 @@
+def example_args(*params):
+    print('Positional argument tuple:', params)
+    
+example_args()
+example_args(1, 2, 4, 'argument')
