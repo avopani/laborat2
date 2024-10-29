f872ead58b5aefa9c588ce14e147c45178ba3808
Nastya Sharipova <stacy00.ru@gmail.com>
2024-10-30 00:12:32 +0500

    Add files via upload

---
+++ homework5/nechet.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 29 23:48:33 2024
+
+@author: stacy
+"""
+
+def get_number():
+    for number in range(30):
+        if number % 2 != 0:  
+            yield number  
+          
+def numb():
+
+    numbers = list(get_number())
+    print('Все нечетные числа:',numbers)  
+    print(f'1-е нечетное число:{numbers[0]};\n 5-е нечетное число: {numbers[4]};\n последнее нечетное число: {numbers[-1]}')
+    
+
+numb()
+
+
